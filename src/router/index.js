d62443f5bef480f81e7af19f8442a9e0152ade82
Timeraa <29104008+Timeraa@users.noreply.github.com>
2019-02-22 21:00:15 +0100

    Remove # from URL

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,38 @@
-import Vue from "vue";
-import Router from "vue-router";
+import Vue from 'vue';
+import Router from 'vue-router';
 Vue.use(Router);
 
 export default new Router({
-    routes: [
-        {
-            path: "",
-            redirect: "/Home"
-        },
-        {
-            path: "/Home",
-            name: "Home",
-            component: require("@/pages/Home").default,
-        },
-        {
-            path: "/Store",
-            name: "Store",
-            component: require("@/pages/Store").default
-        },
-        {
-            path: "/Downloads",
-            name: "Downloads",
-            component: require("@/pages/Downloads").default
-        },
-        {
-            path: "/Contributors",
-            name: "Contributors",
-            component: require("@/pages/Contributors").default
-        },
-        {
-            path: "/Submit",
-            name: "Submit",
-            component: require("@/pages/Submit").default
-        }
-    ]
+	mode: 'history',
+	routes: [
+		{
+			path: '',
+			redirect: '/Home'
+		},
+		{
+			path: '/Home',
+			name: 'Home',
+			component: require('@/pages/Home').default
+		},
+		{
+			path: '/Store',
+			name: 'Store',
+			component: require('@/pages/Store').default
+		},
+		{
+			path: '/Downloads',
+			name: 'Downloads',
+			component: require('@/pages/Downloads').default
+		},
+		{
+			path: '/Contributors',
+			name: 'Contributors',
+			component: require('@/pages/Contributors').default
+		},
+		{
+			path: '/Submit',
+			name: 'Submit',
+			component: require('@/pages/Submit').default
+		}
+	]
 });
