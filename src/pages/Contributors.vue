e48e47281da40b2f2123474c9db2c931c19da6cc
Mulvey <dlmf1ccd@thompsonschools.org>
2019-02-27 12:52:50 -0700

    Contributor Things

--- src/pages/Contributors.vue
+++ src/pages/Contributors.vue
@@ -3,32 +3,35 @@
     <title>PreMiD - Contributors</title>
     <div v-for="contributor of contributors" v-bind:key="contributor" class="contributor">
       <div class="picture"><img :src="contributor.picture" width="100%" height="100%"></div>
-      <span :class="contributor.position"></span>
-      <span class="name">{{ contributor.name }}</span>
+      <span :class="contributor.position">{{ contributor.position.toUpperCase() }}</span>
+      <br>
+      <span class="name">{{ contributor.name.toUpperCase() }}</span>
     </div>
   </div>
 </template>
 
 <script>
+import request from "request";
+
 export default {
   name: "contributors",
   data() {
     return {
       contributors: [
         {
-          name: "TIMERAA#7947",
+          name: "Timeraa#7947",
           picture: "https://cdn.discordapp.com/avatars/223238938716798978/96d2e1c86178c8c79c99577e9772ada8.png",
           position: "developer"
         },
         {
-          name: "FRUXH#3282",
+          name: "Fruxh#3282",
           picture: "https://cdn.discordapp.com/avatars/259407123782434816/a_cd2423939738e2e32713da9da189707d.gif",
           position: "developer"
         },
         {
-          name: "MULVERINEX#0001",
+          name: "MulverineX#0001",
           picture: "https://cdn.discordapp.com/avatars/178551656714076161/a_d0e66e232c888d6f937887ada48eaa4c.gif",
-          position: "developer"
+          position: "contributor"
         },
         {
           name: "Timeraa",
@@ -47,6 +50,11 @@ export default {
         }
       ]
     }
+  },
+  mounted() {
+    request("https://api.premid.app/credits", () => {
+      let data = JSON.parse(dat);
+    })
   }
 };
 </script>
@@ -78,6 +86,7 @@ export default {
     font-family: Inter;
     &.name {
       color: white;
+      font-size: .8rem;
     }
     &.developer {
       color: #349BE1;
