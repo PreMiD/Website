78c66135e8eb4606b98db6a24619455fc18978c7
Timeraa <metz.florian@arcor.de>
2019-06-22 15:45:27 +0200

    🗑 Remove Store references as not out for stable

--- src/components/layout/Navigation.vue
+++ src/components/layout/Navigation.vue
@@ -1,42 +1,47 @@
 <template>
-<div class="navbar__container">
-  <div class="navbar">
-    <router-link to="/home">
-    <div class="navbar__logotype">
-      <img src="./../../assets/images/logo.svg">
-      <p class="logotype__title">PREMID</p>
-      </div>
-    </router-link>
-    <div class="navbar__items on-desktop">
-      <router-link
-        :to="'/' + category.route"
-        v-for="category of categories"
-        v-bind:key="category.route"
-        class="navbar__item"
-      >
-        <i :class="'fas fa-' + category.logo"></i>
-        <span class="item__title">{{ category.title }}</span>
+  <div class="navbar__container">
+    <div class="navbar">
+      <router-link to="/home">
+        <div class="navbar__logotype">
+          <img src="./../../assets/images/logo.svg">
+          <p class="logotype__title">PREMID</p>
+        </div>
       </router-link>
-    </div>
-    <div class="mobile-navbar__menu on-mobile">
-      <a ref="menuTrigger" v-on:click="mobileMenuActive = !mobileMenuActive"><i class="fas fa-bars"></i></a>
-    </div>
-  </div>
-  <transition name="slide-down" mode="out-in">
-      <div v-if="mobileMenuActive" v-on:click="mobileMenuActive = false" class="mobile-navbar__items on-mobile">
+      <div class="navbar__items on-desktop">
         <router-link
-        :to="'/' + category.route"
-        v-for="category of categories"
-        v-bind:key="category.route"
-        
-        class="navbar__item"
-      >
-        <i :class="'fas fa-' + category.logo"></i>
-        <span class="item__title">{{ category.title }}</span>
-      </router-link>
+          :to="'/' + category.route"
+          v-for="category of categories"
+          v-bind:key="category.route"
+          class="navbar__item"
+        >
+          <i :class="'fas fa-' + category.logo"></i>
+          <span class="item__title">{{ category.title }}</span>
+        </router-link>
+      </div>
+      <div class="mobile-navbar__menu on-mobile">
+        <a ref="menuTrigger" v-on:click="mobileMenuActive = !mobileMenuActive">
+          <i class="fas fa-bars"></i>
+        </a>
+      </div>
     </div>
-  </transition>
-</div>
+    <transition name="slide-down" mode="out-in">
+      <div
+        v-if="mobileMenuActive"
+        v-on:click="mobileMenuActive = false"
+        class="mobile-navbar__items on-mobile"
+      >
+        <router-link
+          :to="'/' + category.route"
+          v-for="category of categories"
+          v-bind:key="category.route"
+          class="navbar__item"
+        >
+          <i :class="'fas fa-' + category.logo"></i>
+          <span class="item__title">{{ category.title }}</span>
+        </router-link>
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script>
@@ -46,11 +51,6 @@ export default {
     return {
       mobileMenuActive: false,
       categories: [
-        {
-          logo: "store",
-          route: "store",
-          title: "STORE"
-        },
         {
           logo: "download",
           route: "downloads",
@@ -66,9 +66,9 @@ export default {
   },
   methods: {
     showMobileNavigation() {
-      if (this.$refs.menuMobile.style.display !== 'flex') {
-        this.$refs.menuMobile.style.display = 'flex';
-      } else this.$refs.menuMobile.style.display = 'none';
+      if (this.$refs.menuMobile.style.display !== "flex") {
+        this.$refs.menuMobile.style.display = "flex";
+      } else this.$refs.menuMobile.style.display = "none";
     }
   }
 };
