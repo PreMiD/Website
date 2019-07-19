9451a143649ec38107aa518fa12924d1fbf6ce83
Fruxh <mail4finn@gmail.com>
2019-07-20 01:49:32 +0200

    added legal stuff

--- src/router/router.js
+++ src/router/router.js
@@ -1,39 +1,54 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import Vue from "vue";
+import Router from "vue-router";
 Vue.use(Router);
 
 export default new Router({
-	mode: 'history',
-	routes: [
-		{
-			path: '',
-			name: 'home',
-			component: require('@/pages/Home').default
-		},
-		{
-			// If URL not matching any registered one then we drop user to 404 page.
-			path: '*',
-			redirect: '/'
-		},
-		{
-			path: '/store',
-			name: 'store',
-			component: require('@/pages/Store').default
-		},
-		{
-			path: '/downloads',
-			name: 'downloads',
-			component: require('@/pages/Downloads').default
-		},
-		{
-			path: '/contributors',
-			name: 'contributors',
-			component: require('@/pages/Contributors').default
-		},
-		{
-			path: '/submit',
-			name: 'submit',
-			component: require('@/pages/Submit').default
-		}
-	]
+  mode: "history",
+  routes: [
+    {
+      path: "",
+      name: "home",
+      component: require("@/pages/Home").default
+    },
+    {
+      // If URL not matching any registered one then we drop user to 404 page.
+      path: "*",
+      redirect: "/"
+    },
+    {
+      path: "/store",
+      name: "store",
+      component: require("@/pages/Store").default
+    },
+    {
+      path: "/downloads",
+      name: "downloads",
+      component: require("@/pages/Downloads").default
+    },
+    {
+      path: "/contributors",
+      name: "contributors",
+      component: require("@/pages/Contributors").default
+    },
+    {
+      path: "/submit",
+      name: "submit",
+      component: require("@/pages/Submit").default
+    },
+    {
+      path: "/privacy",
+      name: "privacy",
+      component: require("@/pages/Privacy").default
+    },
+    {
+      path: "/cookies",
+      name: "cookies",
+      component: require("@/pages/Cookies").default
+    },
+    {
+      path: "/tos",
+      name: "tos",
+      component: require("@/pages/ToS").default
+    }
+  ]
 });
