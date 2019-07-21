4646c9715191b924eab33cbfdd97b5e4b92006e1
Fruxh <mail4finn@gmail.com>
2019-07-21 04:26:42 +0200

    code quality improved

--- src/main.js
+++ src/main.js
@@ -1,56 +1,53 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App';
-import router from './router/router.js';
+import Vue from "vue";
+import App from "./App";
+import router from "./router/router.js";
 
 // Tooltips
-import VueTippy from 'vue-tippy';
+import VueTippy from "vue-tippy";
 
 // Notifications
-import VueNoty from 'vuejs-noty'
+import VueNoty from "vuejs-noty";
 
 Vue.use(VueNoty, {
-    timeout: 0,
-    progressBar: false,
-    layout: 'topCenter',
-    theme: 'premid',
-    // animation: {
-    //     open: 'animated fade',
-    //     close: 'animated fade'
-    // }
+  timeout: 0,
+  progressBar: false,
+  layout: "topCenter",
+  theme: "premid"
+  // animation: {
+  //     open: 'animated fade',
+  //     close: 'animated fade'
+  // }
 });
 Vue.use(VueTippy);
 Vue.config.productionTip = false;
 
 Vue.mixin({
-    // All methods that are defined here going to be global, use it carefully!
-    methods: {
-        // /DEBUG MESSAGES
-        debugMessage: function (message) {
-            console.log(
-                '%cPreMiD%c ' + message,
-                'color: #fff; font-weight: 900; padding: 3px 7px; margin: 3px; background: #596cae; border-radius: 50px;',
-                'color: #596cae;'
-            );
-        },
-        errorMessage: function (message) {
-            console.log(
-                '%cPreMiD%c ' + message,
-                'color: #fff; font-weight: 900; padding: 3px 7px; margin: 3px; background: crimson; border-radius: 50px;',
-                'color: crimson;'
-            );
-        },
-        // DEBUG MESSAGES/
-        floatingNotification: function (message) {
-
-        }
-
-    }
+  // All methods that are defined here going to be global, use it carefully!
+  methods: {
+    // /DEBUG MESSAGES
+    debugMessage: function(message) {
+      console.log(
+        "%cPreMiD%c " + message,
+        "color: #fff; font-weight: 900; padding: 3px 7px; margin: 3px; background: #596cae; border-radius: 50px;",
+        "color: #596cae;"
+      );
+    },
+    errorMessage: function(message) {
+      console.log(
+        "%cPreMiD%c " + message,
+        "color: #fff; font-weight: 900; padding: 3px 7px; margin: 3px; background: crimson; border-radius: 50px;",
+        "color: crimson;"
+      );
+    },
+    // DEBUG MESSAGES/
+    floatingNotification: function(message) {}
+  }
 });
 
 /* eslint-disable no-new */
 new Vue({
-    router,
-    render: h => h(App)
+  router,
+  render: h => h(App)
 }).$mount("#app");
