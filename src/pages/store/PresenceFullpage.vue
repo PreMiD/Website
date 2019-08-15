fc10b83e5dd199c63478e17a0c55375f5264323a
Florian Metz <metz.florian@arcor.de>
2019-08-15 17:32:13 +0200

    🦄 Remove console.log

--- src/pages/store/PresenceFullpage.vue
+++ src/pages/store/PresenceFullpage.vue
@@ -280,11 +280,10 @@ export default {
       }
     },
     getContrastYIQ(hexcolor) {
-      var r = parseInt(hexcolor.substr(0, 2), 16);
-      var g = parseInt(hexcolor.substr(2, 2), 16);
-      var b = parseInt(hexcolor.substr(4, 2), 16);
-      console.log(r, g, b);
-      var yiq = (r * 299 + g * 587 + b * 114) / 1000;
+      var r = parseInt(hexcolor.substr(0, 2), 16),
+        g = parseInt(hexcolor.substr(2, 2), 16),
+        b = parseInt(hexcolor.substr(4, 2), 16),
+        yiq = (r * 299 + g * 587 + b * 114) / 1000;
       return yiq >= 128 ? "black" : "white";
     }
   }
