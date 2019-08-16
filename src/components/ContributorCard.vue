e677cc56230f13979077e24af83ec34e6199d934
Florian Metz <metz.florian@arcor.de>
2019-08-16 02:37:43 +0200

    🎨 Whitney, spaces are spaces!

--- src/components/ContributorCard.vue
+++ src/components/ContributorCard.vue
@@ -6,7 +6,9 @@
     <h1
       v-text="contributor.name.length > 16 ? contributor.name.slice(0, 15) + '...' : contributor.name"
     />
-    <h2 v-text="contributor.role" />
+    <h2
+      v-html="contributor.role.replace(' ', '<p style=\'display:inline;line-height:0;\'>&nbsp;</p>')"
+    />
     <span :class="contributor.status" />
     <img :src="contributor.avatar + '?size=128'" draggable="false" />
   </div>
