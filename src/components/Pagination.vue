a6fc3b37aedefc59bc4b58660ba67fc098a30886
Voknehzyr <gamesforevaigor@gmail.com>
2019-10-01 22:18:21 +0300

    :recycle: changed: Now categories are displayed above the presences.

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <router-link class="button button_pagination" :disabled="pageItemNumber == pageNumber" :key="pageItemNumber" v-for="pageItemNumber in pageCount" :to="{ query: {page: pageItemNumber } }">{{ pageItemNumber }}</router-link>
+        <router-link class="button button_pagination" :disabled="pageItemNumber == pageNumber" :key="pageItemNumber" v-for="pageItemNumber in pageCount" :to="{ query: {page: pageItemNumber, category: pageCategory } }">{{ pageItemNumber }}</router-link>
     </div>
 </template>
 <script>
