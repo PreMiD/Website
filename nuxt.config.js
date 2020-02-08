d6e54408121d2c9d9abffb52e031b5b9c28a23ad
Florian Metz <metz.florian@arcor.de>
2020-02-08 21:24:01 +0100

    🐛 Fix missing content size head

--- nuxt.config.js
+++ nuxt.config.js
@@ -70,6 +70,8 @@ module.exports = {
 				}
 			],
 			meta: [
+				{ charset: "utf-8" },
+				{ name: "viewport", content: "width=device-width, initial-scale=1" },
 				{
 					hid: "theme-color",
 					name: "theme-color",
