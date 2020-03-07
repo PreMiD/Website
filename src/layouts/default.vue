913fdfddce6499afc05725c9272b9b63ae44a11a
Florian Metz <me@timeraa.dev>
2020-03-07 13:52:26 +0100

    🚮 Remove unneeded import

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -7,13 +7,14 @@
 				:content="$t('home.languageSelector.tippy')"
 				class="button button--gray button--language-switch"
 				@click="toggleSwitcher"
-				><i class="fa-language fas"></i
-			></a>
+			>
+				<i class="fa-language fas"></i>
+			</a>
 		</div>
 		<!--
       ! Disabled for now as it somehow breaks the page in production
     <LanguageNotification />
-    -->
+		-->
 		<header>
 			<navigation />
 		</header>
@@ -27,44 +28,42 @@
 </template>
 
 <script>
-	import Navigation from "~/components/Navigation.vue";
-	import Footer from "~/components/Footer.vue";
-	import Detection from "~/components/mixins/Detection";
-	import Debug from "~/components/mixins/Debug";
-	import LanguageNotification from "~/components/LanguageNotification";
-	import LanguageSwitcher from "~/components/LanguageSwitcher";
+import Navigation from "~/components/Navigation.vue";
+import Footer from "~/components/Footer.vue";
+import Detection from "~/components/mixins/Detection";
+import Debug from "~/components/mixins/Debug";
+import LanguageSwitcher from "~/components/LanguageSwitcher";
 
-	export default {
-		name: "PremidWeb",
-		components: {
-			Navigation,
-			Footer,
-			LanguageNotification,
-			LanguageSwitcher
-		},
-		mixins: [Debug, Detection],
-		data() {
-			return {
-				switcherVisible: false,
-				ua: process.server ? "" : navigator.userAgent
-			};
-		},
-		created() {
-			this.$root.$data.i18nLanguageList = this.$i18n.availableLocales;
+export default {
+	name: "PremidWeb",
+	components: {
+		Navigation,
+		Footer,
+		LanguageSwitcher
+	},
+	mixins: [Debug, Detection],
+	data() {
+		return {
+			switcherVisible: false,
+			ua: process.server ? "" : navigator.userAgent
+		};
+	},
+	created() {
+		this.$root.$data.i18nLanguageList = this.$i18n.availableLocales;
 
-			if (process.browser) {
-				if (localStorage.language !== undefined) {
-					this.$root.$i18n.locale = localStorage.language;
-				}
-			} else this.$root.$i18n.locale = "en";
-
-			this.$root.$data.navigatorLanguage = this.getBrowserLanguage();
-			this.$root.$data.i18nLanguage = this.getCurrentLanguage();
-		},
-		methods: {
-			toggleSwitcher() {
-				this.$data.switcherVisible = !this.$data.switcherVisible;
+		if (process.browser) {
+			if (localStorage.language !== undefined) {
+				this.$root.$i18n.locale = localStorage.language;
 			}
+		} else this.$root.$i18n.locale = "en";
+
+		this.$root.$data.navigatorLanguage = this.getBrowserLanguage();
+		this.$root.$data.i18nLanguage = this.getCurrentLanguage();
+	},
+	methods: {
+		toggleSwitcher() {
+			this.$data.switcherVisible = !this.$data.switcherVisible;
 		}
-	};
+	}
+};
 </script>
