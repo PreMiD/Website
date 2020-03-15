2bf3a49e7797f789f5c3eeae43bb2a91ae665850
Florian Metz <me@timeraa.dev>
2020-03-15 17:27:49 +0100

    ♻ Some job page things

--- src/components/JobApply.vue
+++ src/components/JobApply.vue
@@ -6,35 +6,31 @@
 				<div class="bottomText">
 					<p>
 						{{
-							$t("partners.apply.loggedUser", {
-								0: `${$auth.user.username}#${$auth.user.discriminator}`
-							})
+						$t("partners.apply.loggedUser", {
+						0: `${$auth.user.username}#${$auth.user.discriminator}`
+						})
 						}}
 					</p>
 					<a href="/logout">{{ $t("partners.apply.notYou") }}</a>
-					<p class="steps">
-						{{ $t("jobs.modal.step", { 0: currentPage + 1 }) }}
-					</p>
+					<p class="steps">{{ $t("jobs.modal.step", { 0: currentPage + 1 }) }}</p>
 				</div>
 			</div>
 			<div class="modal-container" style="background-color: #23272a;">
-				<div class="modal-header" style="margin-bottom: 3em;">
+				<div class="modal-header">
 					<h1>{{ $t("jobs.modal.title", { 0: job.jobName }) }}</h1>
-					<p v-text="error" style="color: red; font-size: 1em; margin: 0;"></p>
 				</div>
 
 				<div class="modal-body">
+					<p v-text="error" style="color: red; font-size: 1em; margin: 0;"></p>
+
 					<First v-if="currentPage == 0" :job="job" />
 					<Questions v-if="currentPage == 1" :job="job" />
 					<div v-if="currentPage == 1" class="jobCheckbox">
 						<!-- TODO: Fix the checkbox. -->
 						<div class="checkbox-switcher">
 							<label>
-								<input
-									type="checkbox"
-									v-model="check"
-								/>
-								<span ref="checkbox" class="checkbox-container"></span>
+								<input type="checkbox" v-model="check" />
+								<span ref="checkbox" class="checkbox-container fa"></span>
 								<p>{{ $t("jobs.modal.notice") }}</p>
 							</label>
 						</div>
@@ -47,20 +43,18 @@
 							type="button"
 							class="button"
 							@click="apply()"
-						>
-							{{ $t("partners.apply.form.button.apply") }}
-						</button>
+						>{{ $t("partners.apply.form.button.apply") }}</button>
 						<button
 							v-if="currentPage != 1"
 							type="button"
 							class="button"
 							@click="currentPage++"
-						>
-							{{ $t("jobs.modal.buttons.next") }}
-						</button>
-						<button type="button" class="button" @click="$emit('close')">
-							{{ $t("jobs.modal.buttons.cancel") }}
-						</button>
+						>{{ $t("jobs.modal.buttons.next") }}</button>
+						<button
+							type="button"
+							class="button"
+							@click="$emit('close')"
+						>{{ $t("jobs.modal.buttons.cancel") }}</button>
 					</div>
 				</div>
 			</div>
@@ -69,64 +63,64 @@
 </template>
 
 <script>
-	import axios from "axios";
-	import VueRecaptcha from "vue-recaptcha";
-	import First from "~/components/steps/First";
-	import Questions from "~/components/steps/Questions";
+import axios from "axios";
+import VueRecaptcha from "vue-recaptcha";
+import First from "~/components/steps/First";
+import Questions from "~/components/steps/Questions";
 
-	export default {
-		name: "JobApply",
-		components: {
-			VueRecaptcha,
-			First,
-			Questions
-		},
-		props: {
-			job: Object
-		},
-		data() {
-			return {
-				showModal: false,
-				check: false,
-				errors: 0,
-				error: "",
-				sitekey: "6LcafeAUAAAAAOH9ukc2DVPpZJQLSWp9_cxKAQXC",
-				currentPage: 0
-			};
-		},
-		methods: {
-			apply() {
-				this.errors = 0;
-				this.job.questions.map(question => {
-					if (!question.response && question.required == true) this.errors++;
-				});
+export default {
+	name: "JobApply",
+	components: {
+		VueRecaptcha,
+		First,
+		Questions
+	},
+	props: {
+		job: Object
+	},
+	data() {
+		return {
+			showModal: false,
+			check: false,
+			errors: 0,
+			error: "",
+			sitekey: "6LcafeAUAAAAAOH9ukc2DVPpZJQLSWp9_cxKAQXC",
+			currentPage: 0
+		};
+	},
+	methods: {
+		apply() {
+			this.errors = 0;
+			this.job.questions.map(question => {
+				if (!question.response && question.required == true) this.errors++;
+			});
 
-				if (!this.check) this.errors++;
+			if (!this.check) this.errors++;
 
-				if (this.errors == 0 && this.check) {
-					axios
-						.post(`${process.env.apiBase}/jobs/apply`, {
-							questions: this.job.questions,
-							discordUser: this.$auth.user
-						})
-						.then(data => console.log(data))
-						.catch(err => console.error(err));
-				} else {
-					this.error = this.$t("jobs.modal.error");
-				}
-			},
-			onSubmit: function() {
-				this.$refs.invisibleRecaptcha.execute();
-			},
-			onVerify: function(response) {
-				this.response = response;
-			},
-			onExpired: function() {
-				console.log("Expired");
-			},
-			resetRecaptcha() {
-				this.$refs.recaptcha.reset();
+			if (this.errors == 0 && this.check) {
+				axios
+					.post(`${process.env.apiBase}/jobs/apply`, {
+						questions: this.job.questions,
+						discordUser: this.$auth.user
+					})
+					.then(data => console.log(data))
+					.catch(err => console.error(err));
+			} else {
+				this.error = this.$t("jobs.modal.error");
 			}
+		},
+		onSubmit: function() {
+			this.$refs.invisibleRecaptcha.execute();
+		},
+		onVerify: function(response) {
+			this.response = response;
+		},
+		onExpired: function() {
+			console.log("Expired");
+		},
+		resetRecaptcha() {
+			this.$refs.recaptcha.reset();
 		}
-	};
+	}
+};
 </script>
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,75 +1,227 @@
 <template>
-	<div>
-		<div class="jobs-container">
-			<div class="jobs-heading">
-				<h1 v-text="$t('jobs.header.title')"></h1>
-				<p v-text="$t('jobs.header.description')"></p>
+	<div id="jobsWrapper">
+		<div id="jobsContainer">
+			<div id="heading" class="pattern">
+				<h1 v-text="$t('jobs.header.title')" />
+				<p v-text="$t('jobs.header.description')" />
 				<img src="https://i.imgur.com/PwSpU7o.png" />
+				<div class="waves-divider waves-divider_bottom">
+					<svg
+						class="wave"
+						version="1.1"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 1440 100"
+						preserveAspectRatio="none"
+					>
+						<path
+							class="wave-animation"
+							d="M826.337463,25.5396311 C670.970254,58.655965 603.696181,68.7870267 447.802481,35.1443383 C293.342778,1.81111414 137.33377,1.81111414 0,1.81111414 L0,150 L1920,150 L1920,1.81111414 C1739.53523,-16.6853983 1679.86404,73.1607868 1389.7826,37.4859505 C1099.70117,1.81111414 981.704672,-7.57670281 826.337463,25.5396311 Z"
+							fill="currentColor"
+						/>
+					</svg>
+				</div>
 			</div>
-			<div class="waves-divider waves-divider_bottom">
-				<svg
-					class="wave"
-					version="1.1"
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 1440 100"
-					preserveAspectRatio="none"
-				>
-					<path
-						class="wave-animation"
-						d="M826.337463,25.5396311 C670.970254,58.655965 603.696181,68.7870267 447.802481,35.1443383 C293.342778,1.81111414 137.33377,1.81111414 0,1.81111414 L0,150 L1920,150 L1920,1.81111414 C1739.53523,-16.6853983 1679.86404,73.1607868 1389.7826,37.4859505 C1099.70117,1.81111414 981.704672,-7.57670281 826.337463,25.5396311 Z"
-						fill="currentColor"
-					/>
-				</svg>
-			</div>
-		</div>
-		<div class="jobs-heading">
-			<div class="openings">
-				<h1 v-text="$t('jobs.openings.title')"></h1>
+			<div id="openings">
+				<h1 v-text="$t('jobs.openings.title')" />
 				<div class="jobs">
 					<Job
 						v-for="job in jobs.filter(j => j.available)"
 						:key="job.jobName"
 						:job="job"
-					></Job>
+						@click="applyModal(job)"
+					/>
 				</div>
 			</div>
 			<div class="benefits">
-				<h1 v-text="$t('jobs.benefits.title')"></h1>
+				<h1 v-text="$t('jobs.benefits.title')" />
 				<Benefit
 					v-for="benefit in benefits"
 					:key="benefit.title"
 					:icon="benefit.icon"
 					:title="$t(benefit.title)"
 					:description="$t(benefit.description)"
-				></Benefit>
+				/>
 			</div>
 		</div>
+		<transition name="slide-down" mode="in-out">
+			<JobApply v-if="showModal" :job="modalJob" @close="showModal = false;toggleScroll();" />
+		</transition>
 	</div>
 </template>
 
-<script>
-	import axios from "axios";
-	import Job from "~/components/Job";
-	import Benefit from "~/components/Benefit";
+<style lang="scss">
+#jobsWrapper {
+	#jobsContainer {
+		h1 {
+			text-align: center;
+			font-size: 2.2em;
 
-	export default {
-		name: "Jobs",
-		components: {
-			Job,
-			Benefit
-		},
-		auth: false,
-		async asyncData() {
-			const jobs = (await axios(`${process.env.apiBase}/jobs`)).data,
-				benefits = (await axios(`${process.env.apiBase}/jobs/benefits`)).data;
+			color: white;
 
-			return {
-				jobs,
-				benefits
-			};
-		},
-		head: {
-			title: "Jobs"
+			margin: 0;
 		}
-	};
+
+		#heading {
+			position: relative;
+			z-index: 1;
+			overflow: hidden;
+			padding: 25px 0 150px;
+
+			&.pattern {
+				background-image: url("../../assets/images/pattern.png");
+				background-size: 300px 240px;
+			}
+
+			display: flex;
+			flex-direction: column;
+			justify-content: space-around;
+			text-align: center;
+			align-items: center;
+
+			h1 {
+				text-transform: uppercase;
+				font-size: 3.5em;
+				color: #7289da;
+				margin: 0;
+			}
+
+			p {
+				font-size: 1.2em;
+				color: white;
+				margin: 0;
+				margin-bottom: 2em;
+			}
+		}
+
+		#openings {
+			margin-bottom: 2em;
+
+			.jobs {
+				display: flex;
+				justify-content: center;
+
+				.job {
+					transition: 0.15s transform ease-out;
+
+					width: 350px;
+					height: 175px;
+					background: #23272a;
+					border-radius: 5px;
+					padding: 1em;
+					display: flex;
+					flex-direction: column;
+					align-items: center;
+					justify-content: space-around;
+					margin: 1em;
+
+					&:hover {
+						transform: translateY(-10px);
+					}
+
+					i {
+						font-size: 64px;
+						color: #7289da;
+						margin-bottom: 0.25em;
+					}
+
+					h1 {
+						color: white;
+						font-size: 1.75em;
+					}
+
+					.button {
+						font-size: 0.9em;
+						padding: 0.5rem 2.3rem;
+					}
+				}
+			}
+		}
+
+		.benefits {
+			display: block;
+			max-width: 900px;
+			text-align: center;
+			margin: 0 auto;
+
+			.benefit {
+				width: 200px;
+				height: 150px;
+				display: inline-block;
+				vertical-align: top;
+				text-align: left;
+				justify-content: space-around;
+				margin: 2em;
+
+				i {
+					font-size: 64px;
+					color: #7289da;
+					margin-bottom: 0.25em;
+				}
+
+				h1 {
+					color: white;
+					font-size: 1.6em;
+					margin: 0;
+					text-align: left;
+				}
+
+				p {
+					color: white;
+					font-size: 0.85em;
+				}
+			}
+		}
+	}
+}
+</style>
+
+<script>
+import axios from "axios";
+import Job from "~/components/Job";
+import Benefit from "~/components/Benefit";
+import JobApply from "~/components/JobApply";
+
+export default {
+	name: "Jobs",
+	components: {
+		Job,
+		Benefit,
+		JobApply
+	},
+	auth: false,
+	data() {
+		return {
+			showModal: false,
+			modalJob: null
+		};
+	},
+	async asyncData() {
+		const data = await Promise.all([
+			axios(`${process.env.apiBase}/jobs`),
+			axios(`${process.env.apiBase}/jobs/benefits`)
+		]);
+		const jobs = data[0].data,
+			benefits = data[1].data;
+
+		return {
+			jobs,
+			benefits
+		};
+	},
+	methods: {
+		applyModal(job) {
+			this.modalJob = job;
+			this.$auth.loggedIn ? (this.showModal = true) : $router.push("/login");
+			this.toggleScroll();
+		},
+		toggleScroll() {
+			this.showModal
+				? document.body.classList.add("no-scroll")
+				: document.body.classList.remove("no-scroll");
+		}
+	},
+	head: {
+		title: "Jobs"
+	}
+};
 </script>
