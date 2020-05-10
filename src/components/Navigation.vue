f4f9378ecf2830ba38e2f9c44c48999e14603c55
The Gamerzs <thgamerzs74@gmail.com>
2020-05-10 10:22:13 +0100

    Update Navigation.vue

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -386,7 +386,7 @@
 					);
 			}
 		},
-		mounted() {
+		beforeMount() {
 			this.$data.isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
 				navigator?.userAgent
 			);
