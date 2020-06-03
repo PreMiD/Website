3fd92dc530ed1dfaf57050db1ec903197683931a
The Gamerzs <thgamerzs74@gmail.com>
2020-06-03 23:21:19 +0100

    :recycle: Fixed styling issues

--- src/pages/bug.vue
+++ src/pages/bug.vue
@@ -15,7 +15,11 @@
 			></h1>
 		</div>
 
-		<div v-if="bugInfo.count > 0">
+		<div
+			style="margin-top: -15px;"
+			class="rab-container"
+			v-if="bugInfo.count > 0"
+		>
 			<div class="rab-container">
 				<h1 class="heading" v-t="'report.overview'">
 					{{ $t("report.overview") }}"
@@ -31,25 +35,53 @@
 			<br />
 			<div class="rab-container">
 				<h1 class="heading" v-t="'report.os'">{{ $t("report.os") }}</h1>
-				<select style="width: 85px;" class="selection" required v-model="Report.os">
-					<option>OS X</option>
+				<select
+					style="width: 120px;"
+					class="selection"
+					required
+					v-model="Report.os"
+				>
+					<option>Mac OS</option>
 					<option>Windows</option>
 					<option>Linux</option>
 				</select>
-				<br>
-				<br>
-				<textarea type="text" maxlength="10" style="width: 81px;" class="breif" required v-model="Report.browserversion"/>
+				<br />
+				<br />
+				<textarea
+					type="text"
+					maxlength="14"
+					style="width: 116px;"
+					class="breif"
+					required
+					placeholder="10.0.19041.264"
+					v-model="Report.browserversion"
+				/>
 			</div>
 			<br />
 			<div class="rab-container">
-				<h1 class="heading" v-t="'report.browser'">{{ $t("report.browser") }}</h1>
-				<select style="width: 85px;" class="selection" required v-model="Report.browser">
-					<option>Chrome</option>
+				<h1 class="heading" v-t="'report.browser'">
+					{{ $t("report.browser") }}
+				</h1>
+				<select
+					style="width: 120px;"
+					class="selection"
+					required
+					v-model="Report.browser"
+				>
+					<option selected>Chrome</option>
 					<option>Firefox</option>
 				</select>
-				<br>
-				<br>
-				<textarea type="text" maxlength="10" style="width: 81px;" class="breif" required v-model="Report.osversion" />
+				<br />
+				<br />
+				<textarea
+					type="text"
+					maxlength="20"
+					style="width: 116px;"
+					class="breif"
+					required
+					placeholder="0.0.0.0"
+					v-model="Report.osversion"
+				/>
 			</div>
 			<br />
 			<div class="rab-container">
@@ -60,28 +92,58 @@
 					style="white-space: pre-wrap;"
 					type="text"
 					class="desc"
-					maxlength="2000"
 					v-model="Report.description"
 					required
 				/>
 			</div>
-				<br>
+			<br />
 			<div class="rab-container" style="margin-bottom: 80px;">
-				<button type="button" class="button" @click="addToDB" v-t="'report.button'">{{ $t("report.button") }}</button>
-
+				<button
+					type="button"
+					class="button"
+					@click="addToDB"
+					v-t="'report.button'"
+				>
+					{{ $t("report.button") }}
+				</button>
 			</div>
 		</div>
-		<div v-if="bugInfo.count === 0" class="rab-container" style="margin-bottom: 300px;">
-			<h1 style="text-align: center;" class="heading" v-t="'report.toomany'">{{ $t('report.toomany') }}</h1>
+		<div
+			v-if="bugInfo.count === 0"
+			class="rab-container"
+			style="margin-bottom: 300px; margin-top: -15px;"
+		>
+			<h1 style="text-align: center;" class="heading" v-t="'report.toomany'">
+				{{ $t("report.toomany") }}
+			</h1>
 		</div>
-		<div v-if="bugInfo.count < 3 && bugInfo.count !== -1" class="rab-container" style="margin-bottom: 80px;">
+		<div
+			v-if="bugInfo.count < 3 && bugInfo.count !== -1"
+			class="rab-container"
+			style="margin-bottom: 80px; margin-top: 50px;"
+		>
 			<h2 v-t="'report.activebugs'">{{ $t("report.activebugs") }}</h2>
 			<table style="width: 100%;">
-				<tr v-for="bug of activeBugs.data"
-				:key="bug.breif"
-				>
-					<td style="vertical-align:top;">{{bug.brief}}</td>
-					<td style="padding-left: 50px;padding-bottom:20px; white-space: pre-wrap;" v-html="bug.description"></td>
+				<tr v-for="bug of activeBugs" :key="bug.breif">
+					<td
+						style="
+							vertical-align: top;
+							color: #c7cfd6;
+							padding-bottom: 50px;
+							width: 25%;
+						"
+					>
+						{{ bug.brief }}
+					</td>
+					<td
+						style="
+							padding-left: 50px;
+							padding-bottom: 50px;
+							white-space: pre-wrap;
+							color: #c7cfd6;
+						"
+						v-html="bug.description"
+					></td>
 				</tr>
 			</table>
 		</div>
@@ -97,7 +159,7 @@
 		data() {
 			return {
 				display: false,
-				bugInfo: {count:-1},
+				bugInfo: { count: -1 },
 				bugCount: "Loading...",
 				Report: {
 					brief: "",
@@ -128,8 +190,15 @@
 					status: this.Report.status,
 					date: this.Report.date
 				};
-				if (this.bugInfo.count === 0) return this.$noty.error(this.$t("report.toomany"))
-				if (!newReport.brief || !newReport.description || !newReport.date || !newReport.system) return this.$noty.error(this.$t("report.error"));
+				if (this.bugInfo.count === 0)
+					return this.$noty.error(this.$t("report.toomany"));
+				if (
+					!newReport.brief ||
+					!newReport.description ||
+					!newReport.date ||
+					!newReport.system
+				)
+					return this.$noty.error(this.$t("report.error"));
 
 				axios
 					.post(
@@ -147,32 +216,36 @@
 		},
 		mounted() {
 			this.$auth.$storage.setUniversal("redirect", "/bug");
-			if(!this.$auth.loggedIn) return this.$router.push("/login");
-			axios.get(`${process.env.apiBase}/bugUserInfo/${this.$auth.$storage._state["_token.discord"]}`).then((data => {
-				if(data.data === null || data.data === undefined || data.data === ""){
-					return this.bugInfo.count = 3;
-				}
-
-				this.bugInfo = data.data;
-				if(data.data.count < 3){
-					axios.get(`${process.env.apiBase}/bugInfo/${this.$auth.$storage._state["_token.discord"]}`).then((data => {
-						if(data.result === null){
-							this.activeBugs[0].brief = null;
-							return this.activeBugs[0].description = null;
-						}
-						this.activeBugs = data;
-					}));
-				} else {
-					return;
-				}
-			}))
+			if (!this.$auth.loggedIn) return this.$router.push("/login");
+			axios
+				.get(
+					`${process.env.apiBase}/bugUserInfo/${this.$auth.$storage._state["_token.discord"]}`
+				)
+				.then(data => {
+					if (
+						data.data === null ||
+						data.data === undefined ||
+						data.data === ""
+					) {
+						return (this.bugInfo.count = 3);
+					}
+					console.log(data.data);
+					this.bugInfo.count = data.data.count;
+					if (data.data.count < 3) {
+						this.activeBugs = data.data.bugs;
+					}
+				})
+				.catch(err => {});
 		},
 		head() {
-			if(this.bugInfo.count === 0) {
+			if (this.bugInfo.count === 0) {
 				this.$noty.error(this.$t("report.toomany.alert"));
-				this.bugCount = this.$t('report.bugcount').replace('{count}', (0));
-			}
-			else this.bugCount = this.$t('report.bugcount').replace('{count}', (this.bugInfo.count));
+				this.bugCount = this.$t("report.bugcount").replace("{count}", 0);
+			} else
+				this.bugCount = this.$t("report.bugcount").replace(
+					"{count}",
+					this.bugInfo.count
+				);
 			return {
 				title: "Report A Bug"
 			};
@@ -183,23 +256,32 @@
 <style lang="scss" scoped>
 	@import "../stylesheets/variables.scss";
 
-	.rab{
+	.rab {
 		.rab-container {
 			padding-left: 25px;
 			padding-right: 25px;
 			padding-top: 25px;
+			padding-bottom: 25px;
 			max-width: 1400px;
 			margin: 0 auto;
+			background: #2f3136;
+			border-radius: 0.5rem;
+			color: white;
 
-			.table{
+			.table {
 				white-space: nowrap;
 				width: auto;
 				height: auto;
 			}
+			textarea::-webkit-scrollbar-thumb {
+				width: 10px;
+			}
+			textarea::-webkit-scrollbar-track {
+				border-radius: 10px;
+			}
 		}
 		textarea[type="text"].breif {
 			height: 1.8rem;
-			padding: 20 20;
 			font-size: 14px;
 			transition: all 300ms ease;
 			border: none;
