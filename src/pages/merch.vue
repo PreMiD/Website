5d5ea5def37fd3514620e0f642fbb7ea86feea59
TheGamerzs <44209534+TheGamerzs@users.noreply.github.com>
2020-06-29 14:25:15 +0100

    Create merch.vue

---
+++ src/pages/merch.vue
@@ -0,0 +1,21 @@
+<template>
+</template>
+
+<style lang="scss" scoped>
+</style>
+
+<script>
+
+	export default {
+		name: "Merch",
+		auth: false,
+		mounted() {
+
+		},
+		methods: {
+		},
+		head: {
+			title: "Merch"
+		}
+	};
+</script>
