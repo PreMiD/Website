c5cb1296fcae7090251e8a38609e0ba4034d0c0b
TheGamerzs <44209534+TheGamerzs@users.noreply.github.com>
2020-07-07 21:45:55 +0100

    Update merch.vue

--- src/pages/merch.vue
+++ src/pages/merch.vue
@@ -3,7 +3,7 @@
 		<div class="merch-container__section merch-container__section_header">
 			<div class="merch-container__header">
 				<div class="header__content">
-					<h1>{{ markdown($t("merch.title")) }}</h1>
+					<h1 v-html="markdown($t('merch.title'))"></h1>
 				</div>
 			</div>
 		</div>
@@ -13,44 +13,46 @@
 				:key="category.name"
 				:class="{ 'pattern' : index % 2 == 0}"
 			>
-				<div
-					v-if="index % 2 == 0"
-					class="waves-divider waves-divider_top pattern"
-					style="position: inherit;"
-				>
-					<svg
-						class="wave"
-						version="1.1"
-						xmlns="http://www.w3.org/2000/svg"
-						style="width:100%;"
-						preserveAspectRatio="none"
+				<div class="category-container__content">
+					<div
+						v-if="index % 2 == 0"
+						class="waves-divider waves-divider_top pattern"
+						style="position: inherit;"
 					>
-						<path
-							class="wave-animation"
-							d="M826.337463,25.5396311 C670.970254,58.655965 603.696181,68.7870267 447.802481,35.1443383 C293.342778,1.81111414 137.33377,1.81111414 0,1.81111414 L0,150 L1920,150 L1920,1.81111414 C1739.53523,-16.6853983 1679.86404,73.1607868 1389.7826,37.4859505 C1099.70117,1.81111414 981.704672,-7.57670281 826.337463,25.5396311 Z"
-							fill="currentColor"
-						/>
-					</svg>
-				</div>
-				<h1>{{category}}</h1>
-				<div
-					v-if="index % 2 == 0"
-					class="waves-divider waves-divider_bottom pattern"
-					style="position: inherit;"
-				>
-					<svg
-						class="wave"
-						version="1.1"
-						xmlns="http://www.w3.org/2000/svg"
-						style="width:100%;"
-						preserveAspectRatio="none"
+						<svg
+							class="wave"
+							version="1.1"
+							xmlns="http://www.w3.org/2000/svg"
+							style="width:100%;"
+							preserveAspectRatio="none"
+						>
+							<path
+								class="wave-animation"
+								d="M826.337463,25.5396311 C670.970254,58.655965 603.696181,68.7870267 447.802481,35.1443383 C293.342778,1.81111414 137.33377,1.81111414 0,1.81111414 L0,150 L1920,150 L1920,1.81111414 C1739.53523,-16.6853983 1679.86404,73.1607868 1389.7826,37.4859505 C1099.70117,1.81111414 981.704672,-7.57670281 826.337463,25.5396311 Z"
+								fill="currentColor"
+							/>
+						</svg>
+					</div>
+					<h1>{{category}}</h1>
+					<div
+						v-if="index % 2 == 0"
+						class="waves-divider waves-divider_bottom pattern"
+						style="position: inherit;"
 					>
-						<path
-							class="wave-animation"
-							d="M826.337463,25.5396311 C670.970254,58.655965 603.696181,68.7870267 447.802481,35.1443383 C293.342778,1.81111414 137.33377,1.81111414 0,1.81111414 L0,150 L1920,150 L1920,1.81111414 C1739.53523,-16.6853983 1679.86404,73.1607868 1389.7826,37.4859505 C1099.70117,1.81111414 981.704672,-7.57670281 826.337463,25.5396311 Z"
-							fill="currentColor"
-						/>
-					</svg>
+						<svg
+							class="wave"
+							version="1.1"
+							xmlns="http://www.w3.org/2000/svg"
+							style="width:100%;"
+							preserveAspectRatio="none"
+						>
+							<path
+								class="wave-animation"
+								d="M826.337463,25.5396311 C670.970254,58.655965 603.696181,68.7870267 447.802481,35.1443383 C293.342778,1.81111414 137.33377,1.81111414 0,1.81111414 L0,150 L1920,150 L1920,1.81111414 C1739.53523,-16.6853983 1679.86404,73.1607868 1389.7826,37.4859505 C1099.70117,1.81111414 981.704672,-7.57670281 826.337463,25.5396311 Z"
+								fill="currentColor"
+							/>
+						</svg>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -130,7 +132,6 @@
 			z-index: 1;
 
 			padding-top: 6em;
-			padding-bottom: 12em;
 		}
 
 		&_downloads {
@@ -174,7 +175,7 @@
 		align-items: flex-start;
 
 		.header__content {
-			width: 55%;
+			width: 100%;
 
 			h1 {
 				font-weight: 800;
@@ -182,60 +183,15 @@
 				font-size: 3.8em;
 				letter-spacing: -3px;
 				text-shadow: none;
-				max-width: 600px;
-			}
-
-			p {
-				font-weight: 500;
-				line-height: 1.1;
-				font-size: 1.5em;
-				max-width: 800px;
+				width: 50%;
+				text-align: left;
 			}
 		}
+	}
 
-		.header__steps {
-			width: 35%;
-
-			h2 {
-				margin-block-start: 0.67em;
-				margin-block-end: 0.67em;
-				margin-inline-start: 0px;
-				margin-inline-end: 0px;
-			}
-
-			ol {
-				counter-reset: myCounter;
-				margin: 0;
-				padding: 0;
-
-				li {
-					display: flex;
-					list-style: none;
-					align-items: center;
-
-					p {
-						font-weight: 500;
-						margin: 12px 38px;
-					}
-				}
-
-				li:before {
-					counter-increment: myCounter;
-					content: counter(myCounter);
-					line-height: 1;
-					text-align: center;
-					width: 16px;
-					height: 16px;
-					padding: 5px;
-					margin-right: 12px;
-					font-weight: bold;
-					position: absolute;
-					display: inline-block;
-					border-radius: 50rem;
-					background: linear-gradient(30deg, $accent-primary, #b3aeff);
-				}
-			}
-		}
+	.category-container__content {
+		width: 90%;
+		display: grid;
 	}
 }
 </style>
