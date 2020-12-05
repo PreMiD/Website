f58f6e2c2b131aad0a8882c953aac33a1271c34e
TheDropX <alex.tulbure4@gmail.com>
2020-12-05 20:28:30 +0200

    :sparkles: Alpha/Beta downloads back to life.

--- src/pages/logout.vue
+++ src/pages/logout.vue
@@ -1,18 +1,24 @@
 <template>
-	<p>Please wait while we are logging you out...</p>
+	<h1 class="text-highlight">Please wait while we are logging you out...</h1>
 </template>
 
 <script>
-export default {
-	created() {
-		this.$auth.logout();
-		this.$router.push("/");
-	},
-	head() {
-		return {
-			title: "Logout",
-			titleTemplate: ""
-		};
-	}
-};
+	export default {
+		created() {
+			this.$auth.logout();
+			this.$router.push("/");
+		},
+		head() {
+			return {
+				title: "Logout",
+				titleTemplate: ""
+			};
+		}
+	};
 </script>
+
+<style lang="scss" scoped>
+	h1 {
+		text-align: center;
+	}
+</style>
