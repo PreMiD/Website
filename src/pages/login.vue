d2372667b02e25439985e9ed3549af95d599a861
TheDropX <alex.tulbure4@gmail.com>
2020-12-06 22:56:47 +0200

    :sparkles: Please work.

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,21 +1,27 @@
 <template>
-	<p>Please wait for the redirection...</p>
+	<h1 class="text-highlight">Redirecting...</h1>
 </template>
 
 <script>
-export default {
-	mounted() {
-		if (this.$auth.loggedIn) {
-			this.$router.push("/");
-			return;
+	export default {
+		mounted() {
+			if (this.$auth.loggedIn) {
+				this.$router.push("/");
+				return;
+			}
+			this.$auth.login("discord");
+		},
+		head() {
+			return {
+				title: "Authorization",
+				titleTemplate: ""
+			};
 		}
-		this.$auth.login("discord");
-	},
-	head() {
-		return {
-			title: "Authorization",
-			titleTemplate: ""
-		};
-	}
-};
+	};
 </script>
+
+<style lang="scss" scoped>
+	h1 {
+		text-align: center;
+	}
+</style>
