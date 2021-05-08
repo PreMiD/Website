d6f326e58816fc34b6f918687f1803f45edffc96
TheGamerzs <44209534+TheGamerzs@users.noreply.github.com>
2021-05-08 19:08:21 +0100

    Combined graphql statements

--- src/pages/users/_userId.vue
+++ src/pages/users/_userId.vue
@@ -3,7 +3,7 @@
 		<div class="userpage-container">
 			<div v-if="error">
 				<span>{{ $t("user.notFound.heading") }}</span>
-				<ul style="max-width: 50%;">
+				<ul style="max-width: 50%">
 					<li>{{ $t("user.notFound.message1") }}</li>
 					<li>{{ $t("user.notFound.message2") }}</li>
 					<li>{{ $t("user.notFound.message3") }}</li>
@@ -48,9 +48,7 @@
 							<i
 								v-if="role == 'Administrator'"
 								v-tippy="{
-									content: $t(
-										'contributors.roles.administrator'
-									),
+									content: $t('contributors.roles.administrator'),
 									placement: 'bottom'
 								}"
 								class="fas fa-user-shield"
@@ -170,7 +168,11 @@
 						@click="showContributions = !showContributions"
 						v-html="tabbify($t('user.switch.contributed'))"
 					></div>
-					<div v-else class="noContributes" v-html="tabbify($t('user.switch.contributed'))"></div>
+					<div
+						v-else
+						class="noContributes"
+						v-html="tabbify($t('user.switch.contributed'))"
+					></div>
 				</h1>
 				<div v-if="!showContributions" class="presence-container">
 					<StoreCard
@@ -180,7 +182,10 @@
 						store-functions="true"
 					/>
 				</div>
-				<div v-else-if="showContributions && userContributions.length > 0" class="presence-container">
+				<div
+					v-else-if="showContributions && userContributions.length > 0"
+					class="presence-container"
+				>
 					<StoreCard
 						v-for="presence of userContributions"
 						:key="presence.service"
@@ -194,12 +199,12 @@
 </template>
 
 <script>
-export default {
-	name: "Userpage",
-	auth: false,
-	async asyncData({ params, app }) {
-		let res = await app.$graphql(
-			`{
+	export default {
+		name: "Userpage",
+		auth: false,
+		async asyncData({ params, app }) {
+			let res = await app.$graphql(
+				`{
 				credits(id: "${params.userId}") {
 					user {
 						name
@@ -210,7 +215,7 @@ export default {
 						name
 					}
 				}
-				presences(author: "${params.userId}") {
+				authorPresences: presences(author: "${params.userId}") {
 					metadata {
 						color
 						service
@@ -223,11 +228,7 @@ export default {
 						}
 					}
 				}
-			}`
-			),
-			contributionsRes = await app.$graphql(
-				`{
-				presences(contributor: "${params.userId}") {
+				contributionsPresences: presences(contributor: "${params.userId}") {
 					metadata {
 						color
 						service
@@ -238,7 +239,7 @@ export default {
 							id
 							name
 						}
-						contributors{
+						contributors {
 							id
 							name
 						}
@@ -247,106 +248,106 @@ export default {
 			}`
 			);
 
-		let user = res.credits[0]?.user || {},
-			userPresences = res.presences.map(p => p.metadata),
-			userContributions = contributionsRes.presences.map(p => p.metadata);
+			let user = res.credits[0]?.user || {},
+				userPresences = res.authorPresences.map(p => p.metadata),
+				userContributions = res.contributionsPresences.map(p => p.metadata);
 
-		user.roles = res.credits[0]?.roles?.map(role => role.name).sort();
+			user.roles = res.credits[0]?.roles?.map(role => role.name).sort();
 
-		user.name =
-			user.name ||
-			userPresences[0]?.author?.name ||
-			userContributions[0]?.contributors.find(user => {
-				if (user.id === params.userId) return user;
-			})?.name ||
-			"";
+			user.name =
+				user.name ||
+				userPresences[0]?.author?.name ||
+				userContributions[0]?.contributors.find(user => {
+					if (user.id === params.userId) return user;
+				})?.name ||
+				"";
 
-		user.tag = user.tag || "????";
+			user.tag = user.tag || "????";
 
-		let error = false;
+			let error = false;
 
-		if (!user.name) error = true;
+			if (!user.name) error = true;
 
-		return {
-			error: error,
-			user: user,
-			showContributions: false,
-			userPresences: userPresences,
-			userContributions: userContributions
-		};
-	},
-	methods: {
-		linkify(pls) {
-			if (!pls.match(/(\[.*?\])/g)) return pls;
-			else
-				return pls.match(/(\[.*?\])/g).map(ch => {
-					return pls.replace(
-						ch,
-						`<a href="https://discord.gg/premid">${ch.slice(
-							1,
-							ch.length - 1
-						)}</a>`
-					);
-				})[0];
+			return {
+				error: error,
+				user: user,
+				showContributions: false,
+				userPresences: userPresences,
+				userContributions: userContributions
+			};
 		},
-		tabbify(pls) {
-			if (!pls.match(/(\[.*?\])/g)) return pls;
-			else if (!this.showContributions)
-				return pls.match(/(\[.*?\])/g).map(ch => {
-					return pls.replace(
-						ch,
-						`<span style="color:#7288da">${
-							ch.slice(1, ch.length - 1).split("/")[0]
-						}</span>`
-					);
-				})[0];
-			else if (this.showContributions)
-				return pls.match(/(\[.*?\])/g).map(ch => {
-					return pls.replace(
-						ch,
-						`<span style="color:#7288da">${
-							ch.slice(1, ch.length - 1).split("/")[1]
-						}</span>`
-					);
-				})[0];
+		methods: {
+			linkify(pls) {
+				if (!pls.match(/(\[.*?\])/g)) return pls;
+				else
+					return pls.match(/(\[.*?\])/g).map(ch => {
+						return pls.replace(
+							ch,
+							`<a href="https://discord.gg/premid">${ch.slice(
+								1,
+								ch.length - 1
+							)}</a>`
+						);
+					})[0];
+			},
+			tabbify(pls) {
+				if (!pls.match(/(\[.*?\])/g)) return pls;
+				else if (!this.showContributions)
+					return pls.match(/(\[.*?\])/g).map(ch => {
+						return pls.replace(
+							ch,
+							`<span style="color:#7288da">${
+								ch.slice(1, ch.length - 1).split("/")[0]
+							}</span>`
+						);
+					})[0];
+				else if (this.showContributions)
+					return pls.match(/(\[.*?\])/g).map(ch => {
+						return pls.replace(
+							ch,
+							`<span style="color:#7288da">${
+								ch.slice(1, ch.length - 1).split("/")[1]
+							}</span>`
+						);
+					})[0];
+			}
+		},
+		head() {
+			return {
+				title: `${
+					!this.error && this.user.name ? this.user.name : "Unknown User"
+				}`,
+				meta: [
+					{
+						hid: "description",
+						name: "description",
+						content: `${
+							!this.error && this.user.name ? this.user.name : "Unknown user"
+						}'s profile.`
+					},
+					{
+						hid: "og:description",
+						property: "og:description",
+						content: `${
+							!this.error && this.user.name ? this.user.name : "Unknown user"
+						}'s profile.`
+					},
+					{
+						hid: "og:title",
+						property: "og:title",
+						content:
+							!this.error && this.user.name ? this.user.name : "Unknown User"
+					},
+					{
+						hid: "og:image",
+						property: "og:image",
+						content:
+							!this.error && this.user.avatar
+								? this.user.avatar
+								: "https://premid.app/assets/images/logo.png"
+					}
+				]
+			};
 		}
-	},
-	head() {
-		return {
-			title: `${
-				!this.error && this.user.name ? this.user.name : "Unknown User"
-			}`,
-			meta: [
-				{
-					hid: "description",
-					name: "description",
-					content: `${
-						!this.error && this.user.name ? this.user.name : "Unknown user"
-					}'s profile.`
-				},
-				{
-					hid: "og:description",
-					property: "og:description",
-					content: `${
-						!this.error && this.user.name ? this.user.name : "Unknown user"
-					}'s profile.`
-				},
-				{
-					hid: "og:title",
-					property: "og:title",
-					content:
-						!this.error && this.user.name ? this.user.name : "Unknown User"
-				},
-				{
-					hid: "og:image",
-					property: "og:image",
-					content:
-						!this.error && this.user.avatar
-							? this.user.avatar
-							: "https://premid.app/assets/images/logo.png"
-				}
-			]
-		};
-	}
-};
+	};
 </script>
