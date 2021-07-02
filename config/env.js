7109aefc7035bf16d1db6787838ce674be9827bb
Timeraa <me@timeraa.dev>
2021-07-03 00:40:45 +0200

    🤞 Docker

--- config/env.js
+++ config/env.js
@@ -1,8 +1,8 @@
 let apiBase, graphQLapiBase;
 
 if (process.env.HOSTNAME) {
-	apiBase = `http://${process.env.HOSTNAME}/v2`;
-	graphQLapiBase = `http://${process.env.HOSTNAME}/v2`;
+	apiBase = `${process.env.HOSTNAME}/v2`;
+	graphQLapiBase = `${process.env.HOSTNAME}/v3`;
 } else {
 	apiBase = "https://api.premid.app/v2";
 	graphQLapiBase = "https://api.premid.app/v3";
