cc820f84d11426f77cdc963f22c1a0246326a5b9
TheGamerzs <44209534+TheGamerzs@users.noreply.github.com>
2021-08-13 22:52:51 +0100

    :coffin: Remove  api v2 proxy

--- config/env.js
+++ config/env.js
@@ -1,14 +1,11 @@
-let apiBase, graphQLapiBase;
+let graphQLapiBase;
 
 if (process.env.HOSTNAME) {
-	apiBase = `${process.env.HOSTNAME}/v2`;
 	graphQLapiBase = `${process.env.HOSTNAME}/v3`;
 } else {
-	apiBase = "https://api.premid.app/v2";
 	graphQLapiBase = "https://api.premid.app/v3";
 }
 
 export default {
-	apiBase,
 	graphQLapiBase
 };
