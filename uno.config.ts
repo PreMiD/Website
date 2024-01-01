8abd49810d31815c2f7b247b91a7085387702132
Florian Metz <me@timeraa.dev>
2024-01-01 03:40:48 +0100

    chore: more usable breakpoints

--- uno.config.ts
+++ uno.config.ts
@@ -3,6 +3,14 @@ import { defineConfig } from "unocss";
 
 export default defineConfig({
 	theme: {
+		breakpoints: {
+			"2xl": "1536px",
+			lg: "1024px",
+			md: "768px",
+			sm: "640px",
+			xl: "1280px",
+			xs: "480px"
+		},
 		colors: {
 			bg: {
 				primary: "hsl(227, 18 %, 8 %)",
