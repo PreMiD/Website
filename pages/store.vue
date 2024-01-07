ffec859ea032adfde0fecc30d05c8f1888a93d39
veryCrunchy <verycrunchydev@gmail.com>
2024-01-08 00:22:03 +0100

    feat: homepage main

--- pages/store.vue
+++ pages/store.vue
@@ -1,11 +1,11 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div
-    class="h-full justify-center items-center flex-wrap justify-content gap-2 flex m-5 mt-20"
-  >
-    ✨store✨
-  </div>
+    <div
+        class="h-full justify-center items-center flex-wrap justify-content gap-2 flex m-5 mt-20"
+    >
+        ✨store✨
+    </div>
 </template>
 
 <style scoped></style>
